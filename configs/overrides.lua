04008e2c5094257a7393d34ab17160911dffffc8
Anes Hodza <anes.hodza@renuo.ch>
2023-06-16 18:26:08 +0200

    Open nvimtree if no file is opened

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -45,6 +45,7 @@ M.mason = {
 M.nvimtree = {
   git = {
     enable = true,
+    ignore = false
   },
 
   renderer = {
--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ vim.cmd('command! -nargs=* T NvimTreeToggle <args>')
 vim.cmd('command! -nargs=* F Telescope find_files <args>')
 vim.cmd('command! -nargs=* B Telescope buffers <args>')
 vim.cmd('command! -nargs=* FF Telescope live_grep <args>')
+vim.cmd([[
+  autocmd StdinReadPre * let s:std_in=1
+  autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NvimTreeToggle | endif
+]])
 
 opt.guicursor = ""
 opt.encoding = "UTF-8"
