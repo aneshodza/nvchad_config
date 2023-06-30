f46d1a5a14365811a517453456a21d872532268e
Anes Hodza <anes.hodza@renuo.ch>
2023-06-30 11:30:42 +0200

    Change telescope setup again

--- init.lua
+++ init.lua
@@ -8,8 +8,8 @@ cmd('command! -nargs=* T NvimTreeToggle <args>')
 cmd('command! -nargs=* F Telescope find_files <args>')
 cmd('command! -nargs=* FA Telescope find_files hidden=true <args>') -- This finds ignored stuff too
 cmd('command! -nargs=* FF Telescope live_grep <args>')
-cmd('command! -nargs=* W Telescope buffers <args>')
-cmd('command! -nargs=* B Telescope git_branches <args>')
+cmd('command! -nargs=* B Telescope buffers <args>')
+cmd('command! -nargs=* GB Telescope git_branches <args>')
 
 -- This opens file tree if no file is specified
 -- vim.cmd([[
