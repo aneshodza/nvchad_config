3d0a1266d118261cd9df3b9f5b00fa2354662e21
Anes Hodza <anes.hodza@renuo.ch>
2023-09-18 11:32:06 +0200

    Update readme viewer

--- init.lua
+++ init.lua
@@ -28,8 +28,9 @@ cmd('command! -nargs=* Fi lua vim.lsp.buf.code_action() <args>')
 
 -- markdown previewer
 g.mkdp_auto_start = 1
-g.mkdp_auto_close = 1
-g.mkdp_page_title = '「${name}」'
+g.mkdp_auto_close = 0
+g.mkdp_page_title = '${name}'
+g.mkdp_refresh_slow = 1
 g.mkdp_theme = 'light'
 
 -- Vimtex configuration
