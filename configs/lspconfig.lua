19597b786a70a82f9e6c3bf6b96be737a54e5ea1
Anes Hodza <anes.hodza04@gmail.com>
2024-11-16 17:01:37 +0100

    Remove deprecated tsserver

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -21,8 +21,6 @@ lspconfig.rust_analyzer.setup({
   root_dir = lspconfig.util.root_pattern("Cargo.toml")
 })
 
-lspconfig.tsserver.setup {}
-
 lspconfig.pyright.setup {
   settings = {
     python = {
