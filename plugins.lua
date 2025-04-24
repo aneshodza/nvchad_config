ea231491e6b6f0f0a9c0a3a717653745391750d3
Anes Hodza <anes.hodza04@gmail.com>
2025-04-24 09:33:51 +0200

    Fix deprecated null-ls plugin

--- plugins.lua
+++ plugins.lua
@@ -6,7 +6,8 @@ local plugins = {
     "neovim/nvim-lspconfig",
     dependencies = {
       {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
+        name = "null-ls.nvim",
         config = function()
           require "custom.configs.null-ls"
         end,
